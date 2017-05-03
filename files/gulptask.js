66268519049c2f28fe26604076f53cc47859a64e
Rudolf Cicko <rudolfcicko2016@gmail.com>
2017-05-03 21:50:29 +0100

    commit

--- files/gulptask.js
+++ files/gulptask.js
@@ -1,6 +1,10 @@
 var gghPages = require('gulp-gh-pages');
-var ghPages = require('gh-pages')\n
+var ghPages = require('gh-pages');
+var fs = require('fs-extra');
 
 gulp.task('deploy-gh-pages', [], function() {
+
+  
+ 
   return gulp.src('./_book/**/*').pipe(gghPages());
 });
