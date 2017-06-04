10021b035f2e8aa9cc186eb42e2159b19f36f0e7
Cicko <rudolfcicko2016@gmail.com>
2017-06-04 13:27:37 +0100

    Fixing plugin contamination

--- files/gulptask.js
+++ files/gulptask.js
@@ -1,13 +1,16 @@
-var gghPages = require('gulp-gh-pages');
-var ghPages = require('gh-pages');
-var fs = require('fs-extra');
 
-gulp.task('deploy-gh-pages', [], function() {
+  (function () {
+  var gghPages = require('gulp-gh-pages');
+  var ghPages = require('gh-pages');
+  var fs = require('fs-extra');
 
-  if (!fs.existsSync('_book')) {
-    exec('gitbook build', function (err, out) {
-      if (!err) return gulp.src('./_book/**/*').pipe(gghPages());
-    });
-  }
-  return gulp.src('./_book/**/*').pipe(gghPages());
-});
+  gulp.task('deploy-gh-pages', [], function() {
+
+    if (!fs.existsSync('_book')) {
+      exec('gitbook build', function (err, out) {
+        if (!err) return gulp.src('./_book/**/*').pipe(gghPages());
+      });
+    }
+    return gulp.src('./_book/**/*').pipe(gghPages());
+  });
+})
