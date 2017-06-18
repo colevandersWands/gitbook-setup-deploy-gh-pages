f5fcadc9669363503e6fa247386a672dfa470c92
Cicko <rudolfcicko2016@gmail.com>
2017-06-18 15:46:12 +0100

    Adding hub to create remote repo

--- files/gulptask.js
+++ files/gulptask.js
@@ -4,14 +4,19 @@
   var ghPages = require('gh-pages');
   var gulp = require('gulp');
   var fs = require('fs-extra');
+  var hub = require('hub');
+
+  var bookName = require('./package.json').name;
 
   gulp.task('deploy-gh-pages', [], function() {
-
     if (!fs.existsSync('_book')) {
+      hub.create(bookName);
       exec('gitbook build', function (err, out) {
         if (!err) return gulp.src('./_book/**/*').pipe(gghPages());
       });
     }
-    return gulp.src('./_book/**/*').pipe(gghPages());
+    else {
+      return gulp.src('./_book/**/*').pipe(gghPages());
+    }
   });
 })
