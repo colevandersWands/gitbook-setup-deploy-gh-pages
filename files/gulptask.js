1ecad158fbf2a6f289b07f2f476ebc9c1f195e6f
Cicko <rudolfcicko2016@gmail.com>
2017-06-24 13:49:10 +0100

    Added inquirer to dependencies

--- files/gulptask.js
+++ files/gulptask.js
@@ -3,17 +3,22 @@
   var gghPages = require('gulp-gh-pages');
   var ghPages = require('gh-pages');
   var gulp = require('gulp');
+  var inquirer = require('inquirer')
   var fs = require('fs-extra');
   var hub = require('hub');
 
   var bookName = require('./package.json').name;
 
   gulp.task('deploy-gh-pages', [], function() {
+    if (fs.existsSync('.git')) {
+      exec()
+    }
     if (!fs.existsSync('_book')) {
-      hub.create(bookName);
-      exec('gitbook build', function (err, out) {
-        if (!err) return gulp.src('./_book/**/*').pipe(gghPages());
-      });
+        exec('gitbook build', function (err, out) {
+          if (!err) return gulp.src('./_book/**/*').pipe(gghPages());
+        });
+
+      })
     }
     else {
       return gulp.src('./_book/**/*').pipe(gghPages());
