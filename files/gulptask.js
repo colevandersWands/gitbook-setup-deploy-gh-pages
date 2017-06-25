51d9132685dacdf7a12ee631607389fdf1e1e692
Rudolf Cicko <Rudolf Cicko>
2017-06-25 11:25:46 +0100

    Adding check remote

--- files/gulptask.js
+++ files/gulptask.js
@@ -8,24 +8,29 @@
   var hub = require('hub');
   var bookName = require('./package.json').name;
 
-  const GithubManager = require('./lib/GithubManager.js');
-
-  var ghManager = new GithubManager();
 
   gulp.task('deploy-gh-pages', [], function() {
     if (!fs.existsSync('.git')) {
       exec('git init');
     }
 
+
     if (!fs.existsSync('_book')) {
         exec('gitbook build', function (err, out) {
           if (!err) return gulp.src('./_book/**/*').pipe(gghPages());
         });
-
-      })
     }
     else {
       return gulp.src('./_book/**/*').pipe(gghPages());
     }
   });
+
+
+  gulp.task('check-remote', [], function () {
+    exec ('git remote -v | cut -f1 | grep gh-pages', function (err, out) {
+        console.log(out);
+        console.log(out.length);
+    })
+  });
+
 })(this);
