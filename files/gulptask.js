5ec58a8f4bbe0ff413a71c3b9ee121183ec5bcc5
Cicko <rudolfcicko2016@gmail.com>
2017-06-28 23:51:27 +0100

    Fixed some stuff

--- files/gulptask.js
+++ files/gulptask.js
@@ -13,19 +13,16 @@
     if (!fs.existsSync('.git')) {
       exec('git init');
     }
-
     var set_remote = true;
-
     exec ('git remote -v | cut -f1 | grep origin', function (err, out) {
-        if (out.length == 0) { // No existe remoto gh-pages
+        if (out.length == 0) { // No existe remoto origin
           set_remote = false;
 	  console.log("You don't have remote repo. Please execute","\x1b[33m"," $gitbook-setup set-remote-repo","\x1b[0m");        
 	}
     });
-
     if (set_remote) {
       if (!fs.existsSync('_book')) {
-        console.log("You don't have your book built. Please execute","\x1b[33m"," $gitbook-setup build","\x1b[0m");
+        console.log("You don't have your book built. Please execute","\x1b[33m"," $gitbook build","\x1b[0m");
       }
       else {
         console.log("Lets push to gh-pages");
